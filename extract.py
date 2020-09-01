9a7092081d7e8304f99b44776aa657bba5d9da0c
Aditya Chakraborty <chakra17@purdue.edu>
2020-09-01 00:25:57 -0400

    Added extraction of fda data

--- extract.py
+++ extract.py
@@ -6,6 +6,7 @@ class Extract:
     def __init__(self):
         self.data_sources = json.load(open('config.json'))
         self.csv_path = self.data_sources['data_sources']['csv']
+        self.fda_path = self.data_sources['data_sources']['fda']
 
 
     def getCSVData(self, csv_name):
@@ -17,4 +18,21 @@ class Extract:
         reader = csv.DictReader(open(self.csv_path[csv_name], mode='r', encoding='utf-8-sig'))
         columns = reader.fieldnames
         data = [tuple([row[col] for col in columns]) for row in reader]
+        return columns, data
+
+    def getFDAData(self, filename):
+        if not os.path.exists(self.fda_path[filename]):
+            raise Exception(f"{self.fda_path[filename]} does not exist")
+
+        data = []
+        columns = []
+        with open(self.fda_path[filename]) as f:
+            firstline = True
+            for line in f:
+                if firstline:
+                    columns = line.strip('\n').split('$')
+                    firstline = False
+                else:
+                    data.append(tuple(line.strip('\n').split('$')))
+
         return columns, data
