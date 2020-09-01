59e916c8c8fad615d40d753a0b9462f606c172a6
Aditya Chakraborty <chakra17@purdue.edu>
2020-09-01 00:26:32 -0400

    Added option to stage fda

--- stage.py
+++ stage.py
@@ -28,21 +28,23 @@ class Stage:
         extractObj = Extract()
         self.qh = QueryHandler()
         try:
-            self.conn = stagedb.connect(db_name = dataSet, user = "postgres", password = "postgres", host="localhost")
+            self.conn = stagedb.connect(db_name = "fdadb", user = "postgres", password = "postgres", host="localhost")
             if dataSource == 'csv':
                 self.columns, self.data = extractObj.getCSVData(dataSet)
+            elif dataSource == 'fda':
+                self.columns, self.data = extractObj.getFDAData(dataSet)
             else:
                 print ("Data Source not supported")
-
-            funcName = "stage_" + dataSource
-            getattr(self, funcName)("crypto_data")
+            funcName = "stage"
+            getattr(self, funcName)("Reaction")  #TODO: Hashmap to track db and tables
         except Exception as e:
             print (e)
         finally:
-            self.conn.close()
+            if self.conn:
+                self.conn.close()
 
 
-    def stage_csv(self, tablename):
+    def stage(self, tablename):
         with self.conn.cursor() as cursor:
             psycopg2.extras.execute_batch(cursor, self.qh.insert_data(tablename, self.columns), self.data)
         self.conn.commit()
@@ -50,15 +52,21 @@ class Stage:
         print(f"{len(self.data)} records inserted successfully into mobile table")
 
 
-
 class QueryHandler:
+    def create_table(self, tablename, **kwargs):
+        attr_str = ""
+        for col_name, props in kwargs.items():
+            attr_str += str(col_name) + " " + props + ","
+
+        query = f"CREATE TABLE {tablename} ({attr_str})"
+
     def insert_data(self, table, columns):
         attr_str = ""
         for col in columns[:len(columns)-1]:
             attr_str += str(col) + ", "
         attr_str += columns[len(columns)-1]
-
         query = f"INSERT INTO {table} ({attr_str}) VALUES (" + "%s,"*(len(columns)-1) + "%s)"
+
         return query
 
     def retreive_data(self, table, columns):
