74cd8703eff94b61444daa602765c3d009096f16
Aditya Chakraborty <chakra17@purdue.edu>
2020-08-31 22:17:45 -0400

    Made extraction from csv faster

--- extract.py
+++ extract.py
@@ -1,28 +1,20 @@
-import pandas as pd
-import requests  # this package is used for fetching data from API
+import os
+import csv
 import json
 
 class Extract:
-
     def __init__(self):
-       # loading our json file here to use it across different class methods
         self.data_sources = json.load(open('config.json'))
-        self.api = self.data_sources['data_sources']['api']
         self.csv_path = self.data_sources['data_sources']['csv']
 
 
-    def getAPISData(self, api_name):
-        # since we have multiple API's (Pollution and Economy Data),
-        # so we can get apt API link by passing in its name in function argument.
-
-        api_url = self.api[api_name]
-        response = requests.get(api_url)
-        # response.json() will convert json data into Python dictionary.
-        return response.json()
-
-
     def getCSVData(self, csv_name):
-        # since we can use multiple CSV data files in future,
-        # so will pass csv name as an argument to fetch the desired CSV data.
-        df = pd.read_csv(self.csv_path[csv_name])
-        return df
+        if not os.path.exists(self.csv_path[csv_name]):
+            raise Exception(f"{self.csv_path[csv_name]} does not exist")
+
+        data = []
+        columns = []
+        reader = csv.DictReader(open(self.csv_path[csv_name], mode='r', encoding='utf-8-sig'))
+        columns = reader.fieldnames
+        data = [tuple([row[col] for col in columns]) for row in reader]
+        return columns, data
