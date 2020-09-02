754618dd0d22a6ddf8ac77d22a07024ebfb7feca
Aditya Chakraborty <chakra17@purdue.edu>
2020-09-02 00:11:27 -0400

    Working schema creation for logging and staging + trigger setting

--- db_setup.py
+++ db_setup.py
@@ -3,7 +3,7 @@ from stage import StageDBConnection
 import psycopg2
 
 class DBSetup:
-    def __init__(self, schema):
+    def __init__(self, schema, logging=None):
         self.qh = QueryHandler()
         stagedb =  StageDBConnection()
         try:
@@ -11,7 +11,7 @@ class DBSetup:
             self.conn.autocommit = True
 
             funcName = "create_schema"
-            getattr(self, funcName)(schema)
+            getattr(self, funcName)(schema, logging)
         except Exception as e:
             print (e)
         finally:
@@ -24,21 +24,38 @@ class DBSetup:
         f.close()
 
 
-    def create_schema(self, schema):
+    def create_schema(self, schema, logging=None):
+
         schemaname = "Staging"
         cursor = self.conn.cursor()
         query = self.qh.create_schema(schemaname)
         cursor.execute(query)
         print (f"Schema {schemaname} created")
+
+        if logging:
+            query = self.qh.create_schema("logging")
+            cursor.execute(query)
+            print ("Logging schema created")
+            query = self.qh.create_table("logging", logging)
+            cursor.execute(query)
+            print ("Logging table created")
+
         for k, v in schema.items():
             query = self.qh.create_table(schemaname, v)
             if query:
                 cursor.execute(query)
                 print (f"{k} table created")
 
+                if logging:
+                    #sql_file = open('sql/change_trigger.sql','r')
+                    #cursor.execute(sql_file.read())
+                    #sql_file.close()
+                    query = self.qh.create_trigger(schemaname, v["table_name"], "change_trigger()")
+                    cursor.execute(query)
+                    print (f"Change trigger created for {schemaname}.{k}")
 
         for k, v in schema.items():
             query = self.qh.insert_foreign_key(schemaname, v)
             if query:
                 cursor.execute(query)
-                print (f"Foreign keys for {k} inserted")
+                print (f"Foreign keys for {schemaname}.{k} inserted")
